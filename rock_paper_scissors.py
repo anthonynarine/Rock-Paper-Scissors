6b3f52b6dd672e2a86b009a584342819e6231d1c
Anthony Narine <anarine83@gmail.com>
2022-09-17 14:16:36 -0400

    Game updated with a plyer Class and  run class

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,30 +1,52 @@
-from doctest import TestResults
-import random
-
-#We need inputs fromt he user as to what their selection will be
-#we also need to have the computer randomly select a game object
-def play():
-    user = input ("What's your Choice?'r' for Rock, 'p' for Paper, 's' for Scissors\n")
-    computer = random.choice(['r', 'p', 's'])
-
-    if user == computer:
-        return "It\'s a tie"
-# We know r>s, s>p, p>r lets build a helper function for this
-    if is_win(user, computer):
-        return "You won!"
-    return "You lost!"
-
-def is_win(player, computer):
-    #this will return true if the player wins
-    #game rules r>s, s>p, p>r
-    if (player == 'r' and computer == 's') or (player == 's' and computer == 'p') or (player == 'p' and computer == 'r'):
-        return True
-
-print(play())
+import sys
+from random import choice
 
 
+class player:
+    def __init__(self):
+        self.player = None
+        self.computer = None
+        self.player_score = 0
+        self.computer_score = 0
+        self.tie = 0
+    
+    def play(self):
+        self.player = input ("\nWhat's your Weapon of Choice? >> 'R' for Rock, 'P' for Paper, 'S' for Scissors: ").lower()
+        self.computer = choice(['r', 'p', 's'])
+        print (f"\nThe Computer selects {self.computer.capitalize()}\n")
 
+        if self.player == self.computer:
+            self.tie += 1
+            print ("It\'s a tie!")
 
+        elif self.player == 'r' and self.computer == 's':
+            print ("You Won!")
+            self.player_score += 1
+        
+        elif self.player == 's' and self.computer == 'p':
+            print ("You Won!")
+            self.player_score += 1
+            
+        elif self.player == 'p' and self.computer == 'r':
+            print ("You Won!")
+            self.player_score += 1   
+        else:
+            print ("You Lost!")
+            self.computer_score += 1
 
-# https://www.youtube.com/watch?v=8ext9G7xspg&t=333s
-#time stamp 22:00 mins
+    def run(self):
+        print(f"\n*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*")
+        print (f"\n\t~ Welcome to the Classic: ROCK ~ Paper ~ Scissors ~\n")
+        print("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*")
+        while True:
+            self.play()
+            print (f"\nPlayer score = {self.player_score}: Computer score = {self.computer_score}: Ties = {self.tie}\n")
+            quit = input ("To quit Enter 'Q' or 'C' to continue: ").capitalize()
+            if quit == "Q":
+                sys.exit()
+            else:
+                continue
+                             
+
+player = player()
+player.run()
